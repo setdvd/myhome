8268542b9ae09657965ef1270ecc237e8dc3d918
max <ternovenko@gmail.com>
2017-08-11 21:24:34 +0200

    catch error in migration to avoid error druing adding column to postgresql there no IF NOT EXISTS on ADD COLUMN

--- src/migrations/1502305252073-add-name-to-sensor.ts
+++ src/migrations/1502305252073-add-name-to-sensor.ts
@@ -3,32 +3,34 @@ import {Client} from "pg";
 
 const dbConfig = config.get("db");
 
-export const up = async (next)=>{
-  const db =  new Client(dbConfig);
-  await db.connect();
-  await db.query(`SET SEARCH_PATH TO general;`);
+export const up = async (next) => {
+    const db = new Client(dbConfig);
+    await db.connect();
+    await db.query(`SET SEARCH_PATH TO general;`);
 
-  await db.query(`
-    alter table t_sensor
-      add column name text;
-  `);
+    try {
+        await db.query(`
+            alter table t_sensor
+              add column name text;
+        `);
+    } catch (e) {
+        console.log(e);
+    }
 
-  await db.end();
-  next();
+    await db.end();
+    next();
 };
 
-export const down = async (next)=>{
-const db =  new Client(dbConfig);
-  await db.connect();
-  await db.query(`SET SEARCH_PATH TO general;`);
+export const down = async (next) => {
+    const db = new Client(dbConfig);
+    await db.connect();
+    await db.query(`SET SEARCH_PATH TO general;`);
 
-  await db.query(`
+    await db.query(`
     alter table t_sensor
       drop column name restrict;
   `);
 
-  await db.end();
-  next();
+    await db.end();
+    next();
 };
-
-
